1df35975607cc0c7c05ba7bd196a8ab2c5bdf4ac
Suyash Singh <suyash.singh@sayf.in>
2022-12-30 04:25:14 +0530

    Fix: Update API View

--- map/views.py
+++ map/views.py
@@ -120,3 +120,18 @@ class LoanApplicationView(generics.GenericAPIView,
     
     def patch(self, request, *args, **kwargs):
         return self.partial_update(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+        queryset = self.model.objects.all()
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
